fe1269b5f36f2c44ea4667e29e6663febeb23a8e
th3d3v3lop3r <cjieren@gmail.com>
2023-08-07 14:01:42 +0800

    added checkWin function to App.js

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ class App extends React.Component {
       cardDeck: makeShuffledDeck(),
       // currCards holds the cards from the current round
       currCards: [],
+      currWinner: "",
+      playerOneWinCount: 0,
+      playerTwoWinCount: 0,
     };
   }
 
@@ -20,6 +23,24 @@ class App extends React.Component {
     this.setState({
       currCards: newCurrCards,
     });
+  };
+
+  checkWin = () => {
+    if (this.state.currCards[0].rank > this.state.currCards[1].rank) {
+      this.setState((prevState) => ({
+        currWinner: "Player One won the round!",
+        playerOneWinCount: prevState.playerOneWinCount + 1,
+      }));
+    } else if (this.state.currCards[0].rank < this.state.currCards[1].rank) {
+      this.setState((prevState) => ({
+        currWinner: "Player Two won the round!",
+        playerTwoWinCount: prevState.playerTwoWinCount + 1,
+      }));
+    } else {
+      this.setState({
+        currWinner: "This round is a draw...",
+      });
+    }
   };
 
   render() {
